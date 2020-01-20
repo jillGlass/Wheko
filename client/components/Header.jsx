32964b30d3124501ccfbd1c8a2cb70f8f6311364
JillGlass <glass.nz@gmail.com>
2020-01-21 11:24:30 +1300

    add footer, need to add flex one column

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import {Navbar, Text} from 'react-bootstrap'
-
+import { Navbar, Text } from "react-bootstrap";
 
 export class Header extends Component {
   render() {
     return (
       <>
-        
-  <Navbar className="justify-content-center backgroundColor fontLarge">
-  <Navbar.Text>Wheko</Navbar.Text>
-  </Navbar>
-  
+        <Navbar className="justify-content-center backgroundColor fontLarge">
+          <Navbar.Text>Wheko</Navbar.Text>
+        </Navbar>
       </>
     );
   }
