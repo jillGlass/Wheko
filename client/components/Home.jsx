618c5d709d3689c00e21fb981c4b9dba56e0a473
JillGlass <glass.nz@gmail.com>
2020-01-21 23:34:22 +1300

    old birdcircle added

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from './Footer'
+import Grid from './Grid'
 import fetch from "../api/birds";
 // import ScrollToTop from './ScrollToTop'
 
