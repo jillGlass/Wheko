68314ce3776bcb346a2603a088eb7560914d56b0
JillGlass <glass.nz@gmail.com>
2020-01-22 13:36:34 +1300

    import db functions

---
+++ server/db/db.js
@@ -0,0 +1,31 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  foundBird,
+  getBirds,
+  resetBirds
+}
+
+function getBirds (db = connection) {
+  return db('birds')
+}
+
+function foundBird (id, db = connection) {
+  return db('birds')
+    .where('bird_id', id)
+    .update(
+      { found: true
+      })
+}
+
+function resetBirds (db = connection) {
+  return db('birds')
+    .where('found', '=', true)
+    .update(
+      {
+        found: false
+      }
+    )
+}
