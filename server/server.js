0941208dc988e98ff24bc39c73125198a62af950
JillGlass <glass.nz@gmail.com>
2020-01-22 13:34:19 +1300

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const express = require('express')
 
 const server = express()
 
+const birds = require('./routes/birdRoutes')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use('/api/v1', birds)
 
 module.exports = server
+
