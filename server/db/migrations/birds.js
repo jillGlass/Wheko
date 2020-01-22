19e5eb99fcb31712b9d5d78fc3c1c5d7ad6c85d8
JillGlass <glass.nz@gmail.com>
2020-01-22 13:42:27 +1300

    add table data

---
+++ server/db/migrations/birds.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('birds', (table) => {
+      table.increments('bird_id')
+      table.string('name')
+      table.string('info')
+      table.string('image')
+      table.boolean('found')
+      table.string('category')
+      table.string('status')
+    })
+  }
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable('birds')
+  }
