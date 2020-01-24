71b47b5055b8178ed166137c778b5f4dde41f783
JillGlass <glass.nz@gmail.com>
2020-01-25 11:47:17 +1300

    style component

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -21,14 +21,14 @@ class Profile extends React.Component {
     const bird = this.state.birds.find(bird => bird.bird_id === Number(id))
     return this.state.birds.length === 0 ? null : (
       <React.Fragment>
-        <Card style={{width: '20rem', margin: '20px'}}>
+        <Card style={{width: 'auto', height: 'auto'}}>
             <Card.Img style={{borderRadius: '0px', borderTopLeftRadius: '0px', borderTopRightRadius: '0px'}} variant="top" src="tui.jpg" />
             <Card.Body style={{ padding: '5px' }}>
-            <Card.Title className='text-center' style={{ margin: '0px' }}>{bird.name}</Card.Title>
-            <ListGroup  className="text-center">
+            <Card.Title className='text-center' style={{ margin: '10px' }}>{bird.name}</Card.Title>
+            <ListGroup  className="text-center" style={{padding:'0px 20px 10px 20px'}}>
               <ListGroupItem style={{padding: '0px', border: 'none' }}>{bird.info} </ListGroupItem>
-              <ListGroupItem style={{padding: '0px', border: 'none' }}>{bird.status} </ListGroupItem>
-              <ListGroupItem style={{padding: '0px', border: 'none' }}>{bird.found} </ListGroupItem>
+              <ListGroupItem style={{padding: '10px', border: 'none', color: 'blue' }}>Status: {bird.status} </ListGroupItem>
+              <ListGroupItem style={{padding: '0px', border: 'none', fontWeight: 'bolder' }}>Found: {bird.found} </ListGroupItem>
             </ListGroup>
 
             </Card.Body>
