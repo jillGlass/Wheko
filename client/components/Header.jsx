64f118164b3917ace8f39a6c99d689bdc484b3e5
JillGlass <glass.nz@gmail.com>
2020-01-28 16:10:28 +1300

    clean up header styling to fit found

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,11 +5,19 @@ export class Header extends Component {
   render() {
     return (
       <>
-        <Navbar fixed="top" className="justify-content-center backgroundColor fontLarge headerHeight">
-          <Navbar.Text>Wheko</Navbar.Text>
-          <Navbar.Text style={{padding: '0px', marginTop: '15px'}}>Total Birds Spotted: {this.props.found}</Navbar.Text>
-          <Navbar.Text style={{padding: '0px', marginBottom: '15px'}}>Bird Species Spotted: {this.props.found}</Navbar.Text>
-          
+        <Navbar
+          fixed="top"
+          className="justify-content-center backgroundColor"
+        >
+          <Container style={{flexDirection: 'column', margin: '0px', padding: '0px'}}>
+          <Navbar.Text className="fontLarge" style={{padding: '0px', MarginBottom: '0px'}}>Wheko</Navbar.Text>
+          <Navbar.Text className="justify-content-end" style={{ padding: "0px" }}>
+            Total Birds Spotted: {this.props.found}
+          </Navbar.Text>
+          <Navbar.Text className="justify-content-end" style={{ padding: "0px" }}>
+            Bird Species Spotted: {this.props.found}
+          </Navbar.Text>
+          </Container>
         </Navbar>
       </>
     );
