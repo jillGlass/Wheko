c3cb88bc93b7d57ccfbb330f5bf6b1207d8d2434
JillGlass <glass.nz@gmail.com>
2020-02-09 11:59:26 +1300

    component structure created

---
+++ client/components/HeaderBox.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+class HeaderBox extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default HeaderBox
