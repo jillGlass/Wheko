1dc5b72c7e15cc5842b93034e677fc04b84f6e38
JillGlass <glass.nz@gmail.com>
2020-02-08 17:32:15 +1300

    add logo

--- client/components/FoundBirdCount.jsx
+++ client/components/FoundBirdCount.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React from "react";
+import { Button } from "react-bootstrap";
 
 class FoundBirdCount extends React.Component {
-    render() {
-        return (
-           <React.Fragment>
-               <div className='found-birds-btn'>{this.props.found} species</div>
-
-           </React.Fragment> 
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="found-birds-btn">
+          <div className="btn-text-count">{this.props.found}</div>{" "}
+          <div className="btn-text">BIRDS  </div>
+          <img className="btn-logo" src="binocsbright.png"/>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default FoundBirdCount
+export default FoundBirdCount;
