5f23df4cd96d7ab7b04e833fcf4afc0ad6d42899
JillGlass <glass.nz@gmail.com>
2020-02-09 12:20:28 +1300

    create history and header boxes and insert

---
+++ client/components/HeaderBoxReset.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+class HeaderBoxReset extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Button variant="outline-secondary">Reset</Button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HeaderBoxReset;
