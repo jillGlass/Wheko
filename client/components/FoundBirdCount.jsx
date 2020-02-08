f59a5d54a3d0ed7ccbb47a9721a899cba0ec9466
JillGlass <glass.nz@gmail.com>
2020-02-08 16:31:56 +1300

    component structure created

---
+++ client/components/FoundBirdCount.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {} from 'react-bootstrap'
+
+class FoundBirdCount extends React.Component {
+    render() {
+        return (
+           <React.Fragment>
+
+           </React.Fragment> 
+        )
+    }
+}
+
+export default FoundBirdCount
