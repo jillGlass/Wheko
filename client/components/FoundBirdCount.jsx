8308307429a04335275b7b422dde2c15210934a3
JillGlass <glass.nz@gmail.com>
2020-02-08 16:44:40 +1300

    props passing to component

--- client/components/FoundBirdCount.jsx
+++ client/components/FoundBirdCount.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 class FoundBirdCount extends React.Component {
     render() {
         return (
            <React.Fragment>
+               <Button variant="primary" className='found-birds-btn'>{this.props.found}</Button>
 
            </React.Fragment> 
         )
