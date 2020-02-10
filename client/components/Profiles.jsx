dc3dac9d2843a7ca298df4fca7836e881150ed84
JillGlass <glass.nz@gmail.com>
2020-02-10 13:19:32 +1300

    props feed through

--- client/components/Profiles.jsx
+++ client/components/Profiles.jsx
@@ -3,15 +3,14 @@ import { Row, Col } from "react-bootstrap";
 import fetch from "../api/birds";
 import foundNumber from "../api/foundNumber";
 
-
 class Profiles extends React.Component {
   state = {
     birds: []
   };
 
   handleClick(id) {
-    foundNumber(id)
-    this.componentDidMount()
+    foundNumber(id);
+    this.componentDidMount();
   }
 
   componentDidMount() {
@@ -26,26 +25,34 @@ class Profiles extends React.Component {
     const { id } = this.props.match.params;
     const bird = this.state.birds.find(bird => bird.bird_id === Number(id));
     return this.state.birds.length === 0 ? null : (
-//bird.number etc to return bird info
+      //bird.number etc to return bird info
       <React.Fragment>
         <div className="container">
-        <div className="row">
+          <div className="row">
             <div className="col-2"></div>
-            <div className="col-8"><img src='tui.jpg' width='850px'/></div>
+            <div className="col-8">
+              <img src="tui.jpg" height="600px" />
+            </div>
             <div className="col-2"></div>
           </div>
           <div className="row">
             <div className="col-2"></div>
             <div className="col-2"></div>
-            <div className="col-4">Hello hello hello hello hello hello hello hello</div>
+            <div className="col-4">
+              <div className="row">{bird.number}</div>
+              <div className="row">{bird.name}</div>
+              <div className="row">Latin</div>
+              <div className="row">
+                <img src="noteturq.png" />
+              </div>
+              <div className="row">{bird.info}</div>
+            </div>
             <div className="col-2"></div>
             <div className="col-2"></div>
           </div>
           <div className="row">
             <div className="col-12"></div>
-            
           </div>
-         
         </div>
       </React.Fragment>
     );
