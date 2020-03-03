192ba2be5c9ca0d825e7aea401abbd38ac7649fc
JillGlass <glass.nz@gmail.com>
2020-03-03 16:12:48 +1300

    add biginter to replace interger in schema

--- server/db/migrations/20200123220723_birds.js
+++ server/db/migrations/20200123220723_birds.js
@@ -1,17 +1,19 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('birds', (table) => {
-        table.increments('bird_id')
-        table.text('name')
-        table.text('info')
-        table.text('image')
-        table.integer('number')
-        table.boolean('found')
-        table.text('category')
-        table.text('status')
-      })
-};
-
-exports.down = function(knex) {
+      table.increments('bird_id')
+      table.string('name')
+      table.text('info')
+      table.string('image')
+      table.bigInteger('number')
+      table.boolean('found')
+      table.string('category')
+      table.string('status')
+    })
+  }
+  
+  exports.down = function (knex) {
     return knex.schema.dropTable('birds')
-};
+  }
+
+//   table.integer('number')
