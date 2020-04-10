abb7ce518ebfab5023c10a77b494651f401f09a0
JillGlass <glass.nz@gmail.com>
2020-04-11 09:25:07 +1200

    state resetting but with no number just blank

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -26,6 +26,8 @@ class App extends React.Component {
       .catch((err) => err.message);
   }
 
+ 
+
   counterFound = (birds) => {
     const number = birds.map((bird) => bird.number);
     const foundnumbers = number.reduce((number, bird) => number + bird, 0);
@@ -43,6 +45,12 @@ class App extends React.Component {
 
   handleClick = () => {
     reset()
+    .then((birds) => {
+      this.setState({
+        found: birds.number, //species found
+        number: birds.found //total birds found
+      });
+    })
     console.log("reset hit");
   };
 
